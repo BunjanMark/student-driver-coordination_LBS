58c1325ce201e8a205fcc2f3b45fc0428864d9d1
BunjanMark <bunjan.mark476@gmail.com>
2023-12-10 16:31:24 +0800

    nativr elemns

--- CLIENT/src/screentabs/Home.js
+++ CLIENT/src/screentabs/Home.js
@@ -1,20 +1,28 @@
 import { View, Text,Button } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
-const Home = () => {
-    const navigator = useNavigation();
-  return (
-    <View>
-      <Text>HomeScreen</Text>
-      <Button title='Go to login'
-        onPress={() => {
-          navigator.navigate("LoginScreen");
-        }}
-      >
-        Log out
-      </Button>
-    </View>
-  )
-}
+import { SearchBar } from 'react-native-elements';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default Home
+export default class App extends React.Component {
+    state = {
+      search: '',
+    };
+  
+    updateSearch = (search) => {
+      this.setState({ search });
+    };
+  
+    render() {
+      const { search } = this.state;
+  
+      return (
+        <SafeAreaView>
+        <SearchBar
+          placeholder="Type Here..."
+          onChangeText={this.updateSearch}
+          value={search}
+        /></SafeAreaView>
+      );
+    }
+  }
