6b002fdc582c54542001da93ea0c12bc2ff1bfe2
BunjanMark <bunjanmark476@gmail.com>
2023-12-29 22:27:31 +0800

    sidebarmenu enhanced

--- CLIENT/src/components/SidebarMenu.js
+++ CLIENT/src/components/SidebarMenu.js
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
   StyleSheet,
   TouchableOpacity,
-  useWindowDimensions,
+  TouchableWithoutFeedback,
 } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 import { SearchBar } from "react-native-elements";
+import { useNavigation, useIsFocused } from "@react-navigation/native";
+import { useDarkMode } from "../components/context/DarkModeContext";
 
 const FixedHeader = () => {
+  const { darkMode } = useDarkMode();
+  const navigator = useNavigation();
+  const isFocused = useIsFocused();
+
   const [search, setSearch] = useState("");
   const [isSidebarOpen, setSidebarOpen] = useState(false);
 
@@ -25,44 +31,90 @@ const FixedHeader = () => {
     setSidebarOpen(!isSidebarOpen);
   };
 
+  const closeSidebar = () => {
+    setSidebarOpen(false);
+  };
+
+  useEffect(() => {
+    if (!isFocused) {
+      setSidebarOpen(false);
+    }
+  }, [isFocused]);
+
   return (
-    <View style={styles.header}>
-      <TouchableOpacity style={styles.sidebarButton} onPress={toggleSidebar}>
-        <Icon name="menu" size={25} color="black" />
-      </TouchableOpacity>
+    <TouchableWithoutFeedback onPress={closeSidebar}>
+      <View style={[styles.header, darkMode && styles.darkHeader]}>
+        <TouchableOpacity style={styles.sidebarButton} onPress={toggleSidebar}>
+          <Icon name="menu" size={25} color={darkMode ? "white" : "black"} />
+        </TouchableOpacity>
 
-      <View style={styles.searchBar}>
-        <SearchBar
-          placeholder="Search..."
-          placeholderTextColor="black"
-          onChangeText={updateSearch}
-          onClear={clearSearch}
-          value={search}
-          containerStyle={styles.searchBarContainer}
-          inputContainerStyle={styles.searchBarInputContainer}
-          searchIcon={{ color: "black", size: 25 }}
-          inputStyle={{ color: "black" }}
-          clearIcon={{ color: "black" }}
-        />
-      </View>
-
-      {isSidebarOpen && (
-        <View style={styles.sidebar}>
-          <TouchableOpacity style={styles.sidebarItem}>
-            <Icon name="home" size={20} color="white" />
-            <Text style={styles.sidebarText}>Home</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.sidebarItem}>
-            <Icon name="information-circle" size={20} color="white" />
-            <Text style={styles.sidebarText}>About</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.sidebarItem}>
-            <Icon name="settings" size={20} color="white" />
-            <Text style={styles.sidebarText}>Settings</Text>
-          </TouchableOpacity>
+        <View style={styles.searchBar}>
+          <SearchBar
+            placeholder="Search..."
+            placeholderTextColor={darkMode ? "white" : "black"}
+            onChangeText={updateSearch}
+            onClear={clearSearch}
+            value={search}
+            containerStyle={styles.searchBarContainer}
+            inputContainerStyle={styles.searchBarInputContainer}
+            searchIcon={{ color: darkMode ? "white" : "black", size: 25 }}
+            inputStyle={{ color: darkMode ? "white" : "black" }}
+            clearIcon={{ color: darkMode ? "white" : "black" }}
+          />
         </View>
-      )}
-    </View>
+
+        {isSidebarOpen && isFocused && (
+          <View style={[styles.sidebar, darkMode && styles.darkSidebar]}>
+            <TouchableOpacity
+              style={styles.sidebarItem}
+              onPress={() => {
+                navigator.navigate("HomeScreen");
+              }}
+            >
+              <Icon
+                name="home"
+                size={20}
+                color={darkMode ? "black" : "white"}
+              />
+              <Text
+                style={[styles.sidebarText, darkMode && styles.darkSidebarText]}
+              >
+                Home
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.sidebarItem}>
+              <Icon
+                name="information-circle"
+                size={20}
+                color={darkMode ? "black" : "white"}
+              />
+              <Text
+                style={[styles.sidebarText, darkMode && styles.darkSidebarText]}
+              >
+                About
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.sidebarItem}
+              onPress={() => {
+                navigator.navigate("Settings");
+              }}
+            >
+              <Icon
+                name="settings"
+                size={20}
+                color={darkMode ? "black" : "white"}
+              />
+              <Text
+                style={[styles.sidebarText, darkMode && styles.darkSidebarText]}
+              >
+                Settings
+              </Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -80,6 +132,10 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     zIndex: 1,
+  },
+  darkHeader: {
+    backgroundColor: "#575757", // Dark mode background color
+    borderColor: "#76737e", // Dark mode border color
   },
   sidebarButton: {
     padding: 15,
@@ -105,24 +161,34 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: 60,
     left: -5,
-    padding: 15,
+    padding: 5,
     zIndex: 1,
-    borderBottomRightRadius: 20,
-    backgroundColor: "rgba(52, 52, 52, 0.8)",
-    width: "60%",
-    height: 500,
+    borderBottomRightRadius: 30,
+    backgroundColor: "#575757",
+    width: "40%",
+    height: 300,
     gap: 50,
+  },
+  darkSidebar: {
+    backgroundColor: "#908d96", // Dark mode background color
+    borderColor: "black", // Dark mode border color
   },
   sidebarItem: {
     flexDirection: "row",
     alignItems: "center",
     borderWidth: 7,
     borderColor: "transparent",
+    marginLeft: -20,
   },
   sidebarText: {
     color: "white",
     marginLeft: 10,
-    fontSize: 16,
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  darkSidebarText: {
+    color: "black", // Dark mode text color
+    fontWeight: "bold",
   },
 });
 
